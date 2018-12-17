3c85796fe75adf0e5de220871bbab8d478055c8a
loai007 <45327841+loai007@users.noreply.github.com>
2018-12-17 17:55:58 +0200

    importing project

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ERE = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxps = new System.Windows.Forms.TextBox();
+            this.textBoxun = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ERE);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBoxps);
+            this.groupBox1.Controls.Add(this.textBoxun);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(78, 83);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(464, 325);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "user details";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // ERE
+            // 
+            this.ERE.AutoSize = true;
+            this.ERE.Location = new System.Drawing.Point(82, 129);
+            this.ERE.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ERE.Name = "ERE";
+            this.ERE.Size = new System.Drawing.Size(0, 25);
+            this.ERE.TabIndex = 7;
+            this.ERE.UseMnemonic = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 44);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxps
+            // 
+            this.textBoxps.Location = new System.Drawing.Point(182, 75);
+            this.textBoxps.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxps.Name = "textBoxps";
+            this.textBoxps.Size = new System.Drawing.Size(148, 31);
+            this.textBoxps.TabIndex = 4;
+            // 
+            // textBoxun
+            // 
+            this.textBoxun.Location = new System.Drawing.Point(182, 31);
+            this.textBoxun.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxun.Name = "textBoxun";
+            this.textBoxun.Size = new System.Drawing.Size(148, 31);
+            this.textBoxun.TabIndex = 3;
+            this.textBoxun.TextChanged += new System.EventHandler(this.textBoxun_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "  ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxps;
+        private System.Windows.Forms.TextBox textBoxun;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label ERE;
+    }
+}
+
---
+++ WindowsFormsApp1/ManagerAddC.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ManagerAddC : Form
+    {
+        public ManagerAddC()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void writeToFile(string line)
+        {
+            StreamWriter sw = new StreamWriter("instructor.txt",true);
+            sw.WriteLine(line);
+            sw.Close();
+        }
+        private void addbut_Click(object sender, EventArgs e)
+        {
+            int flag = 1;
+            string id = idbox.Text;
+            if (id=="")
+                flag = 0;
+            string pass = passbox.Text;
+            if (pass == "")
+                flag = 0;
+            string name = namebox.Text;
+            if (name == "")
+                flag = 0;
+            string last = lastnbox.Text;
+            if (last == "")
+                flag = 0;
+            string dep = depbox.Text;
+            if (dep == "")
+                flag = 0;
+            string cname = cnamebox.Text;
+            if (cname == "")
+                flag = 0;
+            string day = daybox.Text;
+            if (day == "")
+                flag = 0;
+            string time = timebox.Text;
+            if (time == "")
+                flag = 0;
+            char s = ' ';
+            if (flag != 0)
+            {
+                string line = (id + s + pass + s + name + s + last + s + dep + s + cname + s + day + s + time);
+                writeToFile(line);
+                lblero.Text = "added ☺";
+            }
+            else
+            {
+                lblero.Text = "error";
+            }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ManagerMain f4 = new ManagerMain();
+            f4.Show();
+        }
+    }
+}
