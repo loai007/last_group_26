f1bd5005caa0035d66f73d697db3e4b797ee0284
Abedaab1 <45328006+Abedaab1@users.noreply.github.com>
2018-12-18 14:33:01 +0200

    lol

---
+++ WindowsFormsApp1/studentMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class studentMain : Form
+    {
+        public studentMain()
+        {
+            InitializeComponent();
+        }
+
+        private void sst_Click(object sender, EventArgs e)
+        {
+            Schedule sc = new Schedule();
+            sc.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+    }
+}
