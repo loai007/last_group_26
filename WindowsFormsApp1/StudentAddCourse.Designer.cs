dbdb96854b1c5a5963949202ec153be23cb02bfe
saherkh1 <45327854+saherkh1@users.noreply.github.com>
2018-12-26 16:36:47 +0200

    student add course edit

--- WindowsFormsApp1/StudentAddCourse.Designer.cs
+++ WindowsFormsApp1/StudentAddCourse.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.components = new System.ComponentModel.Container();
             this.welcomelbl = new System.Windows.Forms.Label();
-            this.CoursesTable = new System.Windows.Forms.TableLayoutPanel();
             this.courses_studentsTableAdapter1 = new WindowsFormsApp1.collegeDataSetTableAdapters.courses_studentsTableAdapter();
             this.collegeDataSet = new WindowsFormsApp1.collegeDataSet();
             this.coursesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.coursesTableAdapter = new WindowsFormsApp1.collegeDataSetTableAdapters.coursesTableAdapter();
             this.dataGridViewCourses = new System.Windows.Forms.DataGridView();
+            this.textBoxAddCourse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddCourse = new System.Windows.Forms.Button();
+            this.massagelbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.coursesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCourses)).BeginInit();
@@ -50,24 +53,6 @@
             this.welcomelbl.Size = new System.Drawing.Size(255, 25);
             this.welcomelbl.TabIndex = 0;
             this.welcomelbl.Text = "Here is a list of the courses: ";
-            // 
-            // CoursesTable
-            // 
-            this.CoursesTable.ColumnCount = 6;
-            this.CoursesTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.CoursesTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.CoursesTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.CoursesTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.CoursesTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.CoursesTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.CoursesTable.Location = new System.Drawing.Point(31, 70);
-            this.CoursesTable.Name = "CoursesTable";
-            this.CoursesTable.RowCount = 2;
-            this.CoursesTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.CoursesTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.CoursesTable.Size = new System.Drawing.Size(458, 100);
-            this.CoursesTable.TabIndex = 1;
-            this.CoursesTable.Paint += new System.Windows.Forms.PaintEventHandler(this.CoursesTable_Paint);
             // 
             // courses_studentsTableAdapter1
             // 
@@ -90,20 +75,58 @@
             // dataGridViewCourses
             // 
             this.dataGridViewCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewCourses.Location = new System.Drawing.Point(133, 217);
+            this.dataGridViewCourses.Location = new System.Drawing.Point(17, 51);
             this.dataGridViewCourses.Name = "dataGridViewCourses";
             this.dataGridViewCourses.RowTemplate.Height = 24;
-            this.dataGridViewCourses.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewCourses.Size = new System.Drawing.Size(785, 235);
             this.dataGridViewCourses.TabIndex = 2;
             this.dataGridViewCourses.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCourses_CellContentClick);
+            // 
+            // textBoxAddCourse
+            // 
+            this.textBoxAddCourse.Location = new System.Drawing.Point(479, 296);
+            this.textBoxAddCourse.Name = "textBoxAddCourse";
+            this.textBoxAddCourse.Size = new System.Drawing.Size(142, 22);
+            this.textBoxAddCourse.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter the name of the course you would like to add:";
+            // 
+            // AddCourse
+            // 
+            this.AddCourse.Location = new System.Drawing.Point(495, 324);
+            this.AddCourse.Name = "AddCourse";
+            this.AddCourse.Size = new System.Drawing.Size(100, 40);
+            this.AddCourse.TabIndex = 5;
+            this.AddCourse.Text = "Add course";
+            this.AddCourse.UseVisualStyleBackColor = true;
+            this.AddCourse.Click += new System.EventHandler(this.AddCourse_Click);
+            // 
+            // massagelbl
+            // 
+            this.massagelbl.AutoSize = true;
+            this.massagelbl.Location = new System.Drawing.Point(322, 336);
+            this.massagelbl.Name = "massagelbl";
+            this.massagelbl.Size = new System.Drawing.Size(0, 17);
+            this.massagelbl.TabIndex = 6;
             // 
             // StudentAddCourse
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.massagelbl);
+            this.Controls.Add(this.AddCourse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAddCourse);
             this.Controls.Add(this.dataGridViewCourses);
-            this.Controls.Add(this.CoursesTable);
             this.Controls.Add(this.welcomelbl);
             this.Name = "StudentAddCourse";
             this.Text = "StudentAddCourse";
@@ -119,11 +142,14 @@
         #endregion
 
         private System.Windows.Forms.Label welcomelbl;
-        private System.Windows.Forms.TableLayoutPanel CoursesTable;
         private collegeDataSetTableAdapters.courses_studentsTableAdapter courses_studentsTableAdapter1;
         private collegeDataSet collegeDataSet;
         private System.Windows.Forms.BindingSource coursesBindingSource;
         private collegeDataSetTableAdapters.coursesTableAdapter coursesTableAdapter;
         private System.Windows.Forms.DataGridView dataGridViewCourses;
+        private System.Windows.Forms.TextBox textBoxAddCourse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddCourse;
+        private System.Windows.Forms.Label massagelbl;
     }
 }
