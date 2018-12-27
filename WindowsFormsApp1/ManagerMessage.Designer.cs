35189dc9648c2e93862641af662cda109b67f820
Abedaab1 <45328006+Abedaab1@users.noreply.github.com>
2018-12-27 16:34:55 +0200

    manager Message

--- WindowsFormsApp1/ManagerMessage.Designer.cs
+++ WindowsFormsApp1/ManagerMessage.Designer.cs
@@ -34,13 +34,14 @@
             this.collegeDataSet = new WindowsFormsApp1.collegeDataSet();
             this.collegeDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.listView1 = new System.Windows.Forms.ListView();
+            this.Messages = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.collegeDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.collegeDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // Back
             // 
-            this.Back.Location = new System.Drawing.Point(683, 0);
+            this.Back.Location = new System.Drawing.Point(977, -1);
             this.Back.Name = "Back";
             this.Back.Size = new System.Drawing.Size(116, 61);
             this.Back.TabIndex = 0;
@@ -69,18 +70,33 @@
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(208, 101);
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(185, 101);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(832, 395);
+            this.listView1.Size = new System.Drawing.Size(898, 395);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Messages
+            // 
+            this.Messages.Location = new System.Drawing.Point(220, 518);
+            this.Messages.Name = "Messages";
+            this.Messages.Size = new System.Drawing.Size(175, 39);
+            this.Messages.TabIndex = 3;
+            this.Messages.Text = "New Messages";
+            this.Messages.UseVisualStyleBackColor = true;
+            this.Messages.Click += new System.EventHandler(this.Messages_Click);
             // 
             // ManagerMessage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1095, 604);
+            this.Controls.Add(this.Messages);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.Message);
             this.Controls.Add(this.Back);
@@ -100,5 +116,6 @@
         private System.Windows.Forms.BindingSource collegeDataSetBindingSource;
         private collegeDataSet collegeDataSet;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Messages;
     }
 }
--- WindowsFormsApp1/ManagerMessage.cs
+++ WindowsFormsApp1/ManagerMessage.cs
@@ -31,19 +31,28 @@ namespace WindowsFormsApp1
 
             string line = sr.ReadLine();
             string[] details = line.Split(' ');
-            string[] arr = new string[4];
+            string[] arr = new string[3];
+           
             char s = ' ';
             while (line != null)
             {
                 if (id == details[0])
                 {
-                    arr[0] = details[1];
-                    ListViewItem itm;
-                    for(int i=2;i<details.Length;i++)
-                            arr[1]=details[i] + s + details[i+1];
-                    itm = new ListViewItem(arr);
-                    listView1.Items.Add(itm);
+                    arr[0] = "Id: ";
+                    arr[0] += details[1];
+                    arr[1] = "Message: ";
+                    for (int i = 2; i < details.Length ; i++)
+                    {
+                        arr[1] =arr[1]+ details[i] + s;
+                    }
+                    
+                    listView1.Items.Add(arr[0]);
+                    listView1.Items.Add(arr[1]);
+                    
+
                 }
+                arr[0] = "";
+                arr[1] = "";
                 line = sr.ReadLine();
                 if (line != "" && line != null)
                 {
@@ -54,8 +63,13 @@ namespace WindowsFormsApp1
             sr.Close();
             
         }
-
+        
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Messages_Click(object sender, EventArgs e)
         {
             StreamReader mi = new StreamReader("user.txt");
             string line = mi.ReadLine();
