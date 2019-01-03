1a50a62786dcac6a9b1f3b181f16ea41873340cb
saherkh1 <45327854+saherkh1@users.noreply.github.com>
2019-01-04 01:41:57 +0200

    sdf

---
+++ UnitTesting/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var user = new WindowsFormsApp1();
+        }
+    }
+}
