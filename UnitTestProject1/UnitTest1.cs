cb77c612a4403214c154367ea0f2a91c4e86323c
saherkh1 <45327854+saherkh1@users.noreply.github.com>
2019-01-04 01:55:03 +0200

    added unit testing as should

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Form1 asshole = new Form1();
+        }
+    }
+}
