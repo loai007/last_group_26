fec382a77928c00713087f92aa9cb204ef1fec20
saherkh1 <45327854+saherkh1@users.noreply.github.com>
2019-01-17 00:22:18 +0200

    a trick to stay in track

---
+++ WindowsFormsApp1/Auto data forms/MainAuto.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApp1.Auto_data_forms
+{
+    partial class MainAuto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddFileBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.viewRequestsBTN = new System.Windows.Forms.Button();
+            this.SendRequestBTN = new System.Windows.Forms.Button();
+            this.sendMessagesBTN = new System.Windows.Forms.Button();
+            this.viewMessagesBTN = new System.Windows.Forms.Button();
+            this.Exams = new System.Windows.Forms.Button();
+            this.ViewGreadsbtn = new System.Windows.Forms.Button();
+            this.nessagesendBtn = new System.Windows.Forms.Button();
+            this.AddCourseInsBtn = new System.Windows.Forms.Button();
+            this.uperpanel = new System.Windows.Forms.Panel();
+            this.lbl = new System.Windows.Forms.Label();
+            this.uperpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AddFileBtn
+            // 
+            this.AddFileBtn.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddFileBtn.Location = new System.Drawing.Point(16, 212);
+            this.AddFileBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.AddFileBtn.Name = "AddFileBtn";
+            this.AddFileBtn.Size = new System.Drawing.Size(78, 39);
+            this.AddFileBtn.TabIndex = 64;
+            this.AddFileBtn.Text = "add files";
+            this.AddFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Sitka Display", 10.2F);
+            this.button1.Location = new System.Drawing.Point(476, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 37);
+            this.button1.TabIndex = 51;
+            this.button1.Text = "Log out";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // viewRequestsBTN
+            // 
+            this.viewRequestsBTN.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewRequestsBTN.Location = new System.Drawing.Point(26, 323);
+            this.viewRequestsBTN.Margin = new System.Windows.Forms.Padding(2);
+            this.viewRequestsBTN.Name = "viewRequestsBTN";
+            this.viewRequestsBTN.Size = new System.Drawing.Size(92, 58);
+            this.viewRequestsBTN.TabIndex = 72;
+            this.viewRequestsBTN.Text = "View Requests Status";
+            this.viewRequestsBTN.UseVisualStyleBackColor = true;
+            // 
+            // SendRequestBTN
+            // 
+            this.SendRequestBTN.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendRequestBTN.Location = new System.Drawing.Point(161, 323);
+            this.SendRequestBTN.Margin = new System.Windows.Forms.Padding(2);
+            this.SendRequestBTN.Name = "SendRequestBTN";
+            this.SendRequestBTN.Size = new System.Drawing.Size(89, 58);
+            this.SendRequestBTN.TabIndex = 71;
+            this.SendRequestBTN.Text = "Send Request";
+            this.SendRequestBTN.UseVisualStyleBackColor = true;
+            // 
+            // sendMessagesBTN
+            // 
+            this.sendMessagesBTN.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendMessagesBTN.Location = new System.Drawing.Point(311, 323);
+            this.sendMessagesBTN.Margin = new System.Windows.Forms.Padding(2);
+            this.sendMessagesBTN.Name = "sendMessagesBTN";
+            this.sendMessagesBTN.Size = new System.Drawing.Size(88, 58);
+            this.sendMessagesBTN.TabIndex = 70;
+            this.sendMessagesBTN.Text = "Send Messages";
+            this.sendMessagesBTN.UseVisualStyleBackColor = true;
+            // 
+            // viewMessagesBTN
+            // 
+            this.viewMessagesBTN.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewMessagesBTN.Location = new System.Drawing.Point(448, 323);
+            this.viewMessagesBTN.Margin = new System.Windows.Forms.Padding(2);
+            this.viewMessagesBTN.Name = "viewMessagesBTN";
+            this.viewMessagesBTN.Size = new System.Drawing.Size(87, 58);
+            this.viewMessagesBTN.TabIndex = 69;
+            this.viewMessagesBTN.Text = "View Messages";
+            this.viewMessagesBTN.UseVisualStyleBackColor = true;
+            // 
+            // Exams
+            // 
+            this.Exams.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exams.Location = new System.Drawing.Point(472, 212);
+            this.Exams.Margin = new System.Windows.Forms.Padding(2);
+            this.Exams.Name = "Exams";
+            this.Exams.Size = new System.Drawing.Size(80, 39);
+            this.Exams.TabIndex = 68;
+            this.Exams.Text = "Exams";
+            this.Exams.UseVisualStyleBackColor = true;
+            // 
+            // ViewGreadsbtn
+            // 
+            this.ViewGreadsbtn.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ViewGreadsbtn.Location = new System.Drawing.Point(361, 212);
+            this.ViewGreadsbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.ViewGreadsbtn.Name = "ViewGreadsbtn";
+            this.ViewGreadsbtn.Size = new System.Drawing.Size(88, 39);
+            this.ViewGreadsbtn.TabIndex = 67;
+            this.ViewGreadsbtn.Text = "ViewGread";
+            this.ViewGreadsbtn.UseVisualStyleBackColor = true;
+            // 
+            // nessagesendBtn
+            // 
+            this.nessagesendBtn.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nessagesendBtn.Location = new System.Drawing.Point(232, 169);
+            this.nessagesendBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.nessagesendBtn.Name = "nessagesendBtn";
+            this.nessagesendBtn.Size = new System.Drawing.Size(106, 82);
+            this.nessagesendBtn.TabIndex = 66;
+            this.nessagesendBtn.Text = "Message sender";
+            this.nessagesendBtn.UseVisualStyleBackColor = true;
+            this.nessagesendBtn.Click += new System.EventHandler(this.nessagesendBtn_Click);
+            // 
+            // AddCourseInsBtn
+            // 
+            this.AddCourseInsBtn.Font = new System.Drawing.Font("Sitka Display", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddCourseInsBtn.Location = new System.Drawing.Point(120, 169);
+            this.AddCourseInsBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.AddCourseInsBtn.Name = "AddCourseInsBtn";
+            this.AddCourseInsBtn.Size = new System.Drawing.Size(83, 82);
+            this.AddCourseInsBtn.TabIndex = 65;
+            this.AddCourseInsBtn.Text = "Add Course and inst";
+            this.AddCourseInsBtn.UseVisualStyleBackColor = true;
+            this.AddCourseInsBtn.Click += new System.EventHandler(this.AddCourseInsBtn_Click);
+            // 
+            // uperpanel
+            // 
+            this.uperpanel.BackColor = System.Drawing.Color.LightGreen;
+            this.uperpanel.Controls.Add(this.lbl);
+            this.uperpanel.Controls.Add(this.button1);
+            this.uperpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.uperpanel.Location = new System.Drawing.Point(0, 0);
+            this.uperpanel.Margin = new System.Windows.Forms.Padding(2);
+            this.uperpanel.Name = "uperpanel";
+            this.uperpanel.Size = new System.Drawing.Size(571, 82);
+            this.uperpanel.TabIndex = 73;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(218, 37);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(35, 13);
+            this.lbl.TabIndex = 52;
+            this.lbl.Text = "label1";
+            // 
+            // MainAuto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 444);
+            this.Controls.Add(this.AddFileBtn);
+            this.Controls.Add(this.viewRequestsBTN);
+            this.Controls.Add(this.SendRequestBTN);
+            this.Controls.Add(this.sendMessagesBTN);
+            this.Controls.Add(this.viewMessagesBTN);
+            this.Controls.Add(this.Exams);
+            this.Controls.Add(this.ViewGreadsbtn);
+            this.Controls.Add(this.nessagesendBtn);
+            this.Controls.Add(this.AddCourseInsBtn);
+            this.Controls.Add(this.uperpanel);
+            this.Name = "MainAuto";
+            this.Text = "MainAuto";
+            this.uperpanel.ResumeLayout(false);
+            this.uperpanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddFileBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button viewRequestsBTN;
+        private System.Windows.Forms.Button SendRequestBTN;
+        private System.Windows.Forms.Button sendMessagesBTN;
+        private System.Windows.Forms.Button viewMessagesBTN;
+        private System.Windows.Forms.Button Exams;
+        private System.Windows.Forms.Button ViewGreadsbtn;
+        private System.Windows.Forms.Button nessagesendBtn;
+        private System.Windows.Forms.Button AddCourseInsBtn;
+        private System.Windows.Forms.Panel uperpanel;
+        private System.Windows.Forms.Label lbl;
+    }
+}
---
+++ WindowsFormsApp1/Auto data forms/MainAuto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Auto_data_forms
+{
+    public partial class MainAuto : Form
+    {
+        public MainAuto()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Hide();
+        }
+
+        private void AddCourseInsBtn_Click(object sender, EventArgs e)
+        {
+            ManagerAddCourse AA = new ManagerAddCourse();
+
+            AA.addbuttonClick("2", "5", "3", "cpp"
+            , "00000000", "computers", "kh", "sa", "333", "333");
+            lbl.Text = "Couse Added AUTOMATICLY ";
+        }
+
+        private void nessagesendBtn_Click(object sender, EventArgs e)
+        {
+            ManagerSendMessage AA = new ManagerSendMessage();
+
+            AA.click("111", "222", "hi my name is", "manager.txt");
+            AA.click("1111","22", "hi my name is", "instructor.txt");
+            AA.click("222","3333", "hi my name is", "student.txt");
+            lbl.Text = "Auto message sender";
+        }
+    }
+}
